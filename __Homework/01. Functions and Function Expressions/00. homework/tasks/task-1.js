cfd5af8e8a7cc6b6a51880520af8be55d4323e4c
madmarks <bratmiro@gmail.com>
2015-06-17 01:43:58 +0300

    00.homework

---
+++ __Homework/01. Functions and Function Expressions/00. homework/tasks/task-1.js
@@ -0,0 +1,36 @@
+/* Task Description */
+/* 
+	Write a function that sums an array of numbers:
+		numbers must be always of type Number
+		returns `null` if the array is empty
+		throws Error if the parameter is not passed (undefined)
+		throws if any of the elements is not convertible to Number	
+
+*/
+
+function solve() {
+    return function sum(array) {
+        // your solution here 
+    };
+}
+
+function sum(array) {
+    "use strict";
+
+    if (!arguments.length) {
+        throw Error;
+    } else if (!array.length) {
+        return null;
+    }
+
+    return array.reduce(function (sum, item) {
+
+        if (isNaN(item)) {
+            throw Error;
+        }
+
+        return (sum + +item);
+    }, 0);
+}
+
+module.exports = sum;
---
+++ __Homework/01. Functions and Function Expressions/00. homework/tasks/task-2.js
@@ -0,0 +1,69 @@
+/* Task description */
+/*
+	Write a function that finds all the prime numbers in a range
+		1) it should return the prime numbers in an array
+		2) it must throw an Error if any on the range params is not convertible to `Number`
+		3) it must throw an Error if any of the range params is missing
+*/
+
+function solve() {
+    return function findPrimes(from, to) {
+        // your solution here 
+    };
+}
+
+function findPrimes(from, to) {
+    "use strict";
+
+    if (arguments.length < 2) {
+        throw new Error;
+    } else if (isNaN(+from) || isNaN(+to)) {
+        throw new Error;
+    } else {
+        from = +from;
+        to = +to;
+    }
+
+    function isPrime(number) {
+        "use strict";
+
+        var divisor = 2,
+            len = Math.sqrt(number);
+
+        if (number == 2) {
+            return true;
+        } else if (number < 2) {
+            return false;
+        }
+
+        //for (divisor = 2, len = Math.sqrt(number) ; divisor <= len; divisor += 1) {
+        //    if(!(number % divisor))
+        //    {
+        //        return false;
+        //    }
+        //}
+
+        while (divisor <= Math.sqrt(number)) {
+            if (!(number % divisor)) {
+                return false;
+            }
+
+            divisor += 1;
+        }
+        return true;
+    }
+
+    var i,
+    result = [];
+
+    for (i = Math.ceil(from) ; i <= to; i += 1) {
+
+        if (isPrime(i)) {
+            result.push(i);
+        }
+    }
+
+    return result;
+}
+
+module.exports = findPrimes;
