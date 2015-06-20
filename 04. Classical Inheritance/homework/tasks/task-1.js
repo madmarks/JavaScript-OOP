eb41d550bbe820f11fe649db18aefc399af0f81a
madmarks <bratmiro@gmail.com>
2015-06-21 00:57:58 +0300

    04. Classical Inheritance - HomeWork

--- 04. Classical Inheritance/homework/tasks/task-1.js
+++ 04. Classical Inheritance/homework/tasks/task-1.js
@@ -16,12 +16,93 @@
 		*	enables method-chaining
 */
 function solve() {
-	var Person = (function () {
-		function Person() {
-		}
-		
-		return Person;
-	} ());
-	return Person;
+    'use strict';
+
+    var Person = (function () {
+
+        function isValidName(name) {
+
+            // ^ is the begin of string, $ is the end of string. 
+            // It's used here to make sure the complete string does contain characters only. 
+            // Without them any string containing letters would be matched. 
+            // Now strings containing letters ONLY will be matched.
+
+            var regExp = /^[a-zA-Z]+$/,
+                isNameWithLatinLettersOnly = regExp.test(name);
+
+            if (!isNameWithLatinLettersOnly || name.length < 3 || name.length > 20) {
+                return false;
+            }
+
+            return true;
+        }
+
+        function Person(firstname, lastname, age) {
+
+            if (!(this instanceof Person)) {
+                return new Person(firstname, lastname, age);
+            }
+
+            this.firstname = firstname;
+            this.lastname = lastname;
+            this.age = age;
+        }
+
+        Person.prototype = {
+            get firstname() {
+
+                return this._firstname;
+            },
+            set firstname(value) {
+
+                if (!isValidName(value)) {
+                    throw new Error('firstname must always be strings between 3 and 20 characters , containing only Latin letters');
+                }
+
+                this._firstname = value;
+            },
+            get lastname() {
+
+                return this._lastname;
+            },
+            set lastname(value) {
+
+                if (!isValidName(value)) {
+                    throw new Error('lastname must always be strings between 3 and 20 characters , containing only Latin letters');
+                }
+
+                this._lastname = value;
+            },
+            get age() {
+
+                return this._age;
+            },
+            set age(value) {
+
+                if (isNaN(+value) || +value < 0 || +value > 150) {
+                    throw new Error('Age must always be a number in the range 0 - 150');
+                }
+
+                this._age = +value;
+            },
+            get fullname() {
+
+                return this._firstname + ' ' + this._lastname;
+            },
+            set fullname(value) {
+
+                var names = value.split(' ');
+                this._firstname = names[0];
+                this._lastname = names[1];
+            },
+        };
+
+        Person.prototype.introduce = function () {
+            return 'Hello! My name is ' + this.fullname + ' and I am ' + this.age + '-years-old';
+        }
+
+        return Person;
+    }());
+    return Person;
 }
 module.exports = solve;
