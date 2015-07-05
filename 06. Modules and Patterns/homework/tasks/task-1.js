3c171971c90920b1c92accde0282c689693f6e11
madmarks <bratmiro@gmail.com>
2015-07-05 04:49:13 +0300

    06. Modules and Patterns - HomeWork

--- 06. Modules and Patterns/homework/tasks/task-1.js
+++ 06. Modules and Patterns/homework/tasks/task-1.js
@@ -45,23 +45,371 @@
 */
 
 function solve() {
-	var Course = {
-		init: function(title, presentations) {
-		},
-		addStudent: function(name) {
-		},
-		getAllStudents: function() {
-		},
-		submitHomework: function(studentID, homeworkID) {
-		},
-		pushExamResults: function(results) {
-		},
-		getTopStudents: function() {
-		}
-	};
 
-	return Course;
+    //===============================================================================
+    function isString(str) {
+
+        return typeof str === "string" || (typeof str === "object" && str.constructor === String);
+    }
+
+    function isInt(n) {
+        return Number(n) === n && n % 1 === 0;
+    }
+
+    function isFloat(n) {
+        return Number(n) === n && n % 1 !== 0
+    }
+
+    function isStudentNameValid(name) {
+
+        var names,
+            firstname,
+            lastname;
+
+        if (!name || !isString(name)) {
+
+            return false;
+        }
+
+        names = name.split(' ');
+
+        if (names.length !== 2) {
+
+            return false;
+        }
+
+        firstname = names[0];
+        lastname = names[1];
+
+        // ^ is the begin of string, $ is the end of string. 
+        // It's used here to make sure the complete string does contain digits and Latin characters only. 
+        // Without them any string containing letters and digits would be matched. 
+        // Now strings containing letters and digits ONLY will be matched.
+
+        var regExp = /^[a-zA-Z]+$/,
+            isFirstNameContainsOnlyLatinLetters = regExp.test(firstname),
+            isLastNameContainsOnlyLatinLetters = regExp.test(lastname);
+
+        if (!isFirstNameContainsOnlyLatinLetters ||
+            !isLastNameContainsOnlyLatinLetters ||
+            !(firstname.charCodeAt(0) >= 65 && firstname.charCodeAt(0) <= 90) ||
+            !(lastname.charCodeAt(0) >= 65 && lastname.charCodeAt(0) <= 90)) {
+
+            return false;
+        }
+
+        return true;
+    }
+
+    function isCourseTitleValid(title) {
+
+        // This regular expression looks for 2 or more white spaces.
+        var regExp = /\s{2,}/;
+
+        if (!title ||
+            !isString(title) ||
+            title.charAt(0) === ' ' ||
+            title.charAt(title.length - 1) === ' ' ||
+            regExp.test(title)) {
+
+            return false;
+        }
+
+        return true;
+    }
+
+    function isStudentIdValid(id) {
+
+        var that = this;
+
+        if (!id || !isInt(id) || id < 0) {
+
+            return false;
+        }
+
+        var isStudentIdExists = that.students.some(function (item) {
+
+            return item.id === id;
+        });
+
+        if (!isStudentIdExists) {
+
+            return false;
+        }
+
+        return true;
+    }
+
+    function isHomeworkIdValid(id) {
+
+        if (!id || !isInt(id) || id < 0 || id > this.presentations.length) {
+
+            return false;
+        }
+
+        return true;
+    }
+
+    //================================================================================
+
+    var Student = (function () {
+
+        var Student = {
+
+            init: function (firstname, lastname, id) {
+
+                this.firstname = firstname;
+                this.lastname = lastname;
+                this.id = id;
+                this.score = 0;
+                this.finalScore = 0;
+            }
+        };
+
+        Object.defineProperties(Student, {
+
+            firstname: {
+                get: function () {
+
+                    return this._firstname;
+                },
+                set: function (value) {
+
+                    validateStudentName(value);
+                    this._firstname = value;
+                    return this;
+                }
+            },
+            lastname: {
+                get: function () {
+
+                    return this._lastname;
+                },
+                set: function (value) {
+
+                    validateStudentName(value);
+                    this._lastname = value;
+                    return this;
+                }
+            },
+            id: {
+                get: function () {
+
+                    return this._id;
+                },
+                set: function (value) {
+
+                    validateStudentId(value);
+                    this._id = value;
+                }
+            },
+            score: {
+                get: function () {
+
+                    return this._score;
+                },
+                set: function (value) {
+
+                    validateStudentScore(value);
+                    this._score = value;
+                }
+            },
+            finalScore: {
+                get: function () {
+
+                    return this._finalScore;
+                },
+                set: function (value) {
+
+                    this._finalScore = value;
+                }
+            }
+        });
+
+        function validateStudentName(name) {
+
+            var namePattern = /^[A-Z]{1}[a-z]*/;
+
+            if (!namePattern.test(name)) {
+
+                throw new Error('You have entered an invalid name!');
+            }
+        }
+
+        function validateStudentId(id) {
+
+            if (!id || !isInt(id) || id < 0) {
+
+                throw new Error('This Student ID is wrong!');
+            }
+        }
+
+        function validateStudentScore(score) {
+
+            if (isNaN(score) || score < 0) {
+
+                throw new Error('This Student score is wrong!');
+            }
+        }
+
+        return Student;
+
+    }());
+
+    var Course = {
+
+        init: function (title, presentations) {
+
+            this.title = title;
+            this.presentations = presentations;
+            this.students = [];
+            this.homeworks = [];
+
+            return this;
+        },
+        addStudent: function (name) {
+
+            var student,
+                names,
+                firstname,
+                lastname,
+                studentID,
+                that = this;
+
+            if (!isStudentNameValid(name)) {
+
+                throw new Error('');
+            }
+
+            names = name.split(' ');
+
+            firstname = names[0];
+            lastname = names[1];
+            studentID = that.students.length + 1;
+
+            student = Object.create(Student);
+            student.init(firstname, lastname, studentID);
+
+            that.students.push(student);
+
+            return student.id;
+        },
+        getAllStudents: function () {
+
+            return this.students.slice(0);
+        },
+        submitHomework: function (studentID, homeworkID) {
+
+            if (!isStudentIdValid.call(this, studentID) || !isHomeworkIdValid.call(this, homeworkID)) {
+
+                throw new Error('');
+            }
+
+            this.homeworks[studentID] = this.homeworks[studentID] || [];
+
+            this.homeworks[studentID].push(homeworkID);
+
+            return this;
+        },
+        pushExamResults: function (examResults) {
+
+            var that = this;
+
+            examResults.forEach(function (examResult) {
+
+                var id = examResult.StudentID;
+                var score = examResult.score;
+
+                if (!isStudentIdValid.call(that, +id) || isNaN(+score)) {
+
+                    throw new Error('');
+                }
+
+                that.students.forEach(function (student) {
+
+                    if (student.id === id) {
+
+                        if (student.score !== 0) {
+
+                            throw new Error('Student trying to cheat!');
+
+                        } else {
+
+                            student.score = score;
+                        }
+                    }
+                });
+            });
+
+            return that;
+        },
+        getTopStudents: function () {
+
+            var that = this;
+
+            that.students.forEach(function (student) {
+
+                var numberOfHomeworks = that.homeworks[student.id].length,
+                    homeworksScore = 0.25 * (numberOfHomeworks / that.homeworks.length),
+                    examScore = 0.75 * student.score;
+
+                student.finalScore = examScore + homeworksScore;
+            });
+
+            function compareScores(x, y) {
+
+                return y.finalScore - x.finalScore;
+            }
+
+            return that.students.sort(compareScores).slice(0, 10);
+        },
+        //===============================
+        get title() {
+
+            return this._title;
+        },
+        set title(value) {
+
+            if (!value || !isCourseTitleValid(value)) {
+
+                throw new Error('Course title is invalid!');
+            }
+
+            this._title = value;
+        },
+        get presentations() {
+
+            return this._presentations;
+        },
+        set presentations(value) {
+
+            if (!value || !value.length || !value.every(function (item) {
+
+                return isCourseTitleValid(item);
+
+            })) {
+
+                throw new Error();
+            }
+
+            this._presentations = value;
+        },
+        get homeworks() {
+
+            return this._homeworks;
+        },
+        set homeworks(value) {
+
+            if (!Array.isArray(value)) {
+
+                throw new Error();
+            }
+
+            this._homeworks = value;
+        }
+    }
+
+    return Course;
 }
-
 
 module.exports = solve;
